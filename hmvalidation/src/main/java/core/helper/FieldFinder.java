1efe64d74a688c6fc5ba5633fd3650ff15433f4d
annguyen011197 <1234567minhan>
2019-01-03 03:45:10 +0700

    Get field by path

---
+++ hmvalidation/src/main/java/core/helper/FieldFinder.java
@@ -0,0 +1,33 @@
+package core.helper;
+
+import core.annotation.NotNull;
+
+import java.lang.reflect.Field;
+
+public class FieldFinder {
+    public static Object find(String path, Object src){
+        String[] fieldsNames = path.split("\\.");
+        Class<?> targetClass = src.getClass();
+        for(String fieldName: fieldsNames){
+            try {
+                Field field =  getFieldByName(targetClass,fieldName);
+                targetClass = field.getType();
+                src = getFieldValue(src,field);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return src;
+            }
+        }
+        return src;
+    }
+
+    private static Field getFieldByName(Class<?> targetClass, String fieldName)
+            throws Exception {
+        return targetClass.getDeclaredField(fieldName);
+    }
+
+    private static Object getFieldValue(Object obj, Field field) throws Exception {
+        field.setAccessible(true);
+        return field.get(obj);
+    }
+}
